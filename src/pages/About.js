6fbe7b0da8e7464d7ac1491e676646654c02f583
nearcatch <692914+nearcatch@users.noreply.github.com>
2022-06-14 14:50:45 -0500

    move about blurb into separate import

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,15 +1,12 @@
 import React from "react";
+import about from "../data/about"
 
 export default function About() {
   return (
     <section id="about_me">
       <h2>About Me</h2>
       <p>
-        Jason is a full-stack developer at the beginning of his career. He is
-        currently attending Northwestern University's Coding Bootcamp. His most
-        recent project was creating a weather dashboard to display the current
-        and forecasted weather for a requested location. He has experience in
-        HTML, CSS, JavaScript, jQuery, and API consumption.
+        {about}
       </p>
     </section>
   );

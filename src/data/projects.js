a749403d991517df6264a5a26965592d225f91e7
nearcatch <692914+nearcatch@users.noreply.github.com>
2022-06-14 15:12:32 -0500

    move projects array to import

---
+++ src/data/projects.js
@@ -0,0 +1,41 @@
+// images for projects
+import project1 from "../images/project1.png";
+import hw6 from "../images/hw6.png";
+import hw5 from "../images/hw5.png";
+import hw4 from "../images/hw4.png";
+import hw3 from "../images/hw3.png";
+
+const projects = [
+  {
+    name: "Quick Movie Lookup",
+    link: "https://ggrove87.github.io/nu-project-1/",
+    repo: "https://github.com/ggrove87/nu-project-1",
+    image: project1,
+  },
+  {
+    name: "Weather Dashboard",
+    link: "https://nearcatch.github.io/nu-hw6-weather-dashboard/",
+    repo: "https://github.com/nearcatch/nu-hw6-weather-dashboard",
+    image: hw6,
+  },
+  {
+    name: "Work Day Scheduler",
+    link: "https://nearcatch.github.io/nu-hw5-work-day-scheduler/",
+    repo: "https://github.com/nearcatch/nu-hw5-work-day-scheduler",
+    image: hw5,
+  },
+  {
+    name: " Code Quiz",
+    link: "https://nearcatch.github.io/nu-hw4-code-quiz/",
+    repo: "https://github.com/nearcatch/nu-hw4-code-quiz",
+    image: hw4,
+  },
+  {
+    name: "Password Generator",
+    link: "https://nearcatch.github.io/nu-hw-03-password-generator/",
+    repo: "https://github.com/nearcatch/nu-hw-03-password-generator",
+    image: hw3,
+  },
+];
+
+export default projects;
